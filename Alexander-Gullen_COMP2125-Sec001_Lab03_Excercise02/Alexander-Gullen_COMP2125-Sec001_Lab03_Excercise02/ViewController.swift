dbb07a03afde92ae79365d20f3bbd36348125ef4
Alexander Gullen <200480229@ba-a251-2314.student.georgianc.on.ca>
2023-07-10 16:22:33 -0400

    initial commit

---
+++ Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise02/Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise02
+//
+//  Created by Alexander Gullen on 2023-07-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
