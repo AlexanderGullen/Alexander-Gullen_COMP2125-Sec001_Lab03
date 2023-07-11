7f2626be6a210b08ca9642318513fc6f6558db85
Alexander Gullen <200480229@ba-lc5-2401.student.georgianc.on.ca>
2023-07-11 17:09:25 -0400

    started Excercise01 frontends

---
+++ Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise01/Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise01/ClientViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ClientViewController.swift
+//  Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise01
+//
+//  Created by Alexander Gullen on 2023-07-11.
+//
+
+import UIKit
+
+class ClientViewController: UIViewController {
+
+    @IBOutlet weak var onBack: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise01/Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise01/LoginViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LoginViewController.swift
+//  Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise01
+//
+//  Created by Alexander Gullen on 2023-07-11.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var txb_loginUsername: UITextField!
+    @IBOutlet weak var txb_loginPassword: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLogin(_ sender: Any) {
+        if let inputUsername = txb_loginUsername.text {
+            if let inputPassword = txb_loginPassword.text {
+                if(inputUsername.trimmingCharacters(in: .whitespacesAndNewlines) != "" && inputPassword.trimmingCharacters(in: .whitespacesAndNewlines) != ""){
+                    print("segue performed");
+                    performSegue(withIdentifier: "segueToClientView", sender: self)
+                }else{
+                    print("segue not performed");
+                }
+            }
+        }
+        
+        
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
