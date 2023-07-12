e9a9521beaf76450e6c919319e4319bf2342184f
Alexander Gullen <200480229@ba-a251-2314.student.georgianc.on.ca>
2023-07-12 11:34:46 -0400

    finalized GUI and added functionality

--- Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise02/Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise02/ViewController.swift
+++ Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise02/Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise02/ViewController.swift
@@ -13,15 +13,35 @@ class ViewController: UIViewController {
     
     var billAmount: Double = 0.0;
     
-    var customTipPercentage: Double = 18;
+    var customTipPercentage: Double = 0.18;
 
     //inputs
     @IBOutlet weak var txbBillAmount: UITextField!
 
     @IBOutlet weak var sldTipPercentage: UISlider!
     
+    @IBAction func txbBillAmountEditedChanged(_ sender: Any) {
+        // update the bill amount
+        if let newBillAmount = Double(txbBillAmount.text!){
+            // if the new bill amount specified is valid then set it to be the new billAmount
+            billAmount = newBillAmount
+        }else{
+            // if the new bill amount specified is invalid then do nothing and use the old bill value
+        }
+        
+        // update the lables to reflect the new amount
+        updateLabels()
+    }
+    
     @IBAction func sldValueChanged(_ sender: Any) {
-        print(sldTipPercentage.value)
+        
+        print((sldTipPercentage.value * 100) / 100);
+        // update the rounded tip amount
+        customTipPercentage = Double(round(sldTipPercentage.value * 100) / 100);
+        
+        // update the lables to reflect the new amount
+        updateLabels()
+        
     }
     
     //labels
@@ -36,12 +56,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblCustomTotal: UILabel!
     
     
+    //this function updates all labels in the program
+    func updateLabels() -> Void{
+        
+        //update the slider value
+        sldTipPercentage.value = Float(customTipPercentage);
+        
+        let customTipPercentageString = round(customTipPercentage * 100).description + "%"
+        
+        lblTipPercentage.text = customTipPercentageString;
+        
+        
+        // update 15% tip and total labels
+        let defaultTip = round(billAmount * 0.15 * 100) / 100;
+        lblDefaultTip.text = "$" + String(defaultTip);
+        lblDefaultTotal.text = "$" + String(defaultTip + billAmount);
+        
+        // update custom tip and total labels
+        let customTip = round(billAmount * customTipPercentage * 100) / 100;
+        lblCustomLabel.text = customTipPercentageString;
+        lblCustomTip.text = "$" + String(customTip);
+        lblCustomTotal.text = "$" + String(customTip + billAmount)
+        
+        
+    }
     
     
     override func viewDidLoad() {
         sldTipPercentage.value = Float(customTipPercentage);
-        lblTipPercentage.text = customTipPercentage.description + "%";
-        lblCustomLabel.text = customTipPercentage.description + "%";
+        txbBillAmount.text = billAmount.description
+        updateLabels()
         
         super.viewDidLoad()
         // Do any additional setup after loading the view.
