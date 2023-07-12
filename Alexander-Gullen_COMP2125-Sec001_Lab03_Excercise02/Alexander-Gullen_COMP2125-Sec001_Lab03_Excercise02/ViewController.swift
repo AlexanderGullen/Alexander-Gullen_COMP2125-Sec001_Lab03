e611d3fbef5ee7f56de04a6a37d07e2d0c789bc3
Alexander Gullen <200480229@ba-a251-2314.student.georgianc.on.ca>
2023-07-12 10:28:45 -0400

    Excercise02 Controller References added successfuly

--- Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise02/Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise02/ViewController.swift
+++ Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise02/Alexander-Gullen_COMP2125-Sec001_Lab03_Excercise02/ViewController.swift
@@ -8,8 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //variables
+    
+    var billAmount: Double = 0.0;
+    
+    var customTipPercentage: Double = 18;
 
+    //inputs
+    @IBOutlet weak var txbBillAmount: UITextField!
+
+    @IBOutlet weak var sldTipPercentage: UISlider!
+    
+    @IBAction func sldValueChanged(_ sender: Any) {
+        print(sldTipPercentage.value)
+    }
+    
+    //labels
+    
+    @IBOutlet weak var lblTipPercentage: UILabel!
+    
+    @IBOutlet weak var lblDefaultTip: UILabel!
+    @IBOutlet weak var lblDefaultTotal: UILabel!
+    
+    @IBOutlet weak var lblCustomLabel: UILabel!
+    @IBOutlet weak var lblCustomTip: UILabel!
+    @IBOutlet weak var lblCustomTotal: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
+        sldTipPercentage.value = Float(customTipPercentage);
+        lblTipPercentage.text = customTipPercentage.description + "%";
+        lblCustomLabel.text = customTipPercentage.description + "%";
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
